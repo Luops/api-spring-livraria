89a087cb63ddc85810cb96f0a37f5ef32f48adc9
gabeFrancisco <gabegit2@gmail.com>
2025-03-29 23:13:53 -0300

    created User model

---
+++ src/main/java/com/example/demo/Product/Model/UserModel.java
@@ -0,0 +1,33 @@
+package com.example.demo.Product.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String address;
+
+    @Override
+    public String toString() {
+        return "UserModel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
