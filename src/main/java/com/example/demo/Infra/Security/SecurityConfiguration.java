f33e75eb552819f6efac65483d35491778071bcf
gabeFrancisco <gabegit2@gmail.com>
2025-04-02 14:25:44 -0300

    first successful test with register endpoint

--- src/main/java/com/example/demo/Infra/Security/SecurityConfiguration.java
+++ src/main/java/com/example/demo/Infra/Security/SecurityConfiguration.java
@@ -3,9 +3,13 @@ package com.example.demo.Infra.Security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,14 +17,27 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain  securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-        return  httpSecurity
-            .csrf(csrf -> csrf.disable())
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers(HttpMethod.POST, "/api/v1/users/login").permitAll()
-                .anyRequest().authenticated()
-            )
-            .build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/api/v1/users/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/users/register").permitAll()
+                        .anyRequest().authenticated())
+                .build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
 }
--- src/main/java/com/example/demo/User/Controller/UserController.java
+++ src/main/java/com/example/demo/User/Controller/UserController.java
@@ -2,26 +2,58 @@ package com.example.demo.User.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.User.Model.UserModel;
+import com.example.demo.User.Model.DTO.UserAuthDTO;
+import com.example.demo.User.Model.DTO.UserRegisterDTO;
+import com.example.demo.User.Repository.UserRepository;
+import com.example.demo.User.Service.UserService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
-
 @RestController
 @RequestMapping(path = "api/v1/users")
 public class UserController {
-    
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping
     public String getText() {
         return new String("Ok!!!");
     }
 
     @PostMapping("login")
-    public String postMethodName() {
-        //TODO: process POST request
-        
-        return "Login is working!"; 
+    public ResponseEntity postMethodName(@RequestBody @Valid UserAuthDTO data) {
+        var emailPassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
+        var auth = this.authenticationManager.authenticate(emailPassword);
+
+        return ResponseEntity.ok().build();
     }
-    
+
+    @PostMapping("register")
+    public ResponseEntity postMethodName(@RequestBody @Valid UserRegisterDTO data) {
+        if (this.userRepository.findByEmail(data.email()) != null)
+            return ResponseEntity.badRequest().build();
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        UserModel newUser = new UserModel(data.name(), data.email(), encryptedPassword, data.address());
+
+        this.userRepository.save(newUser);
+
+        return ResponseEntity.ok().build();
+    }
+
 }
